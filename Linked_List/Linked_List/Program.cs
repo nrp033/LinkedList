81f1f51a04d6ed945c4ff46e621744cdf1fbcbd4
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-18 22:07:03 +0530

    Creatiing Simple Linked List Using Predefined Class

---
+++ Linked_List/Linked_List/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t----- Welcome To LinkedList -----");
+
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+            foreach(int item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
